23b3c6d6d3d3749ba77b80236e08cad08911ddaa
Jeremy North-Lewis <northewis.jeremy@gmail.com>
2015-06-13 19:27:44 -0400

    Added a contact section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,35 @@
 $(function (){
 
-	// var fullImg = $('.photoThumbnails a').data('full');
-	//Get all links so we can loop through
-	var $galleryImages = $('.photoThumbnails a');
+	
+	// FANCYBOX
 
-	// console.log($galleryImages);
-	//1. when user clicks on thumbnail show modal box
-	$galleryImages.on('click', function(e){
-		e.preventDefault();
-		var index = $galleryImages.index($(this));
-		//Get full source
-		var src = $(this).data('full');
-		//Get full description
-		var decp = $(this).data('desc')
-
-
-		$('.modalContent img').attr('src',src).data('index',index);
-		
-		$('.overlay').addClass('open');
-
-		$('.selected').removeClass('selected');
-		$(this).addClass('selected');
-
-	//2. have modal box get full-size image
-		// the second src (without apostrophes) is the variable
-
-		var $minHeight = 800;
-		var $height = $('.modal').height();
-		if ( $('.modal').$height > $minHeight) {
-			//$(this).css("width", "40%");
+	$(".fancybox").fancybox({
+		padding : 30,
+		closeBtn : false,
+		nextClick : true,
+		preload : 20,
+		nextSpeed : 150,
+		prevSpeed : 150,
+		openEffect : 'fade',
+		closeEffect : 'fade',
+		openSpeed : 'fast',
+		closeSpeed : 'fast',
+		nextEffect : 'fade',
+		prevEffect : 'fade',
+		keys: {
+			next : {
+				39 : 'left', // right arrow
+				40 : 'up'    // down arrow
+			},
+			prev : {
+				37 : 'right',  // left arrow
+				38 : 'down'    // up arrow
+			},
+			close  : [27], // escape key
 		}
-
-		// CLOSE MENUS WHEN MODAL IS OPEN
-		$('ul.gallery-links, ul.contact-link').css('display', 'none');
 	});
 
-
-	//3. make directional arrows work
-		$('a.arrowRight').on('click', function(e){
-			e.preventDefault();
-			var $selected = $('.selected')
-			if ($selected.is(':first-child')) {
-				$('.photoThumbnails a:last-child').click();	
-			} else {
-				$selected.prev().trigger('click');
-			}
-		
-		});
-
-		$('a.arrowLeft').on('click', function (e){
-			e.preventDefault();
-			var $selected = $('.selected');
-			if ($selected.is(':last-child')) {
-				$('.photoThumbnails a:first-child').click();
-			} else {
-				$selected.next().trigger('click');
-			}
-		});
-
-		// arrow keys
-
-		$(document).keydown(function(e) {
-	        var $current = $('.selected');
-	        if (e.keyCode ==37)
-	        	$current.prev().trigger('click');
-	        else if (e.keyCode ==39)
-	        	$current.next().trigger('click');
-	        else if (e.keyCode ==27)
-	        	$('.overlay').removeClass('open');
-       });
-
-	//4. transitions 
-
-	//5. click on body to close modal
-		
-
-		$('.overlayBKG').on('click', function(){
-				$('.overlay').removeClass('open');
-			
-		});
-
-	/////////// DROP DOWN NAVIGATION ///////////
-	///////////////////////////////////////////
+	// DROP-DOWN NAVIGATION
 
 	$('a.gallery-link').on('click', function(e) {
 		e.preventDefault();
@@ -90,18 +38,25 @@ $(function (){
 	});
 
 	// CONTACT
-	$('a.email-link').on('click', function(e) {
-		e.preventDefault();
-		$('ul.contact-link').slideToggle(350);
-		$('ul.gallery-links').css('display', 'none');
+
+	// $('a.email-link').on('click', function(e) {
+	// 	e.preventDefault();
+	// 	$('ul.contact-link').slideToggle(350);
+	// 	$('ul.gallery-links').css('display', 'none');
+	// });
+
+	$("a.contact").on('click', function(){
+		$(".overlay").fadeToggle(200);
+	});
+
+	$('.overlay').on('click', function(){
+		$(this).fadeToggle(200);   
 	});
 
 	// KEEP GALLERY MENU OPEN ON MUSIC PAGE
 
 	if($('body').hasClass('body-music')) {
 		$('ul.gallery-links').css('display', 'block');
-	} else {
-
 	}
 
 }); // END DOCUMENT READY
